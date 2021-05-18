a4d2e04ac9d489701f93229bb31b84e64c0b5141
Pavel Bartsits <pavelbartsits@gmail.com>
2021-05-18 17:40:22 +0300

    modal window checks functionality

--- src/export_as_masks.py
+++ src/export_as_masks.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import supervisely_lib as sly
 from supervisely_lib.io.json import dump_json_file
-from PIL.Image import MAX_IMAGE_PIXELS
+from PIL import Image
 
 
 task_id = os.environ["TASK_ID"]
@@ -14,16 +14,19 @@ DEBUG_APP_DIR = str(os.environ["DEBUG_APP_DIR"])
 
 my_app = sly.AppService()
 
-MAX_IMAGE_PIXELS = 16000 * 16000
-raise_error = True
-HUMAN_MASKS = True
-MACHINE_MASKS = True
-thickness = 1
+Image.MAX_IMAGE_PIXELS = 16000 * 16000
+PIL_ERROR = os.environ['modal.state.raiseError']
+HUMAN_MASKS = os.environ['modal.state.humanMasks']
+MACHINE_MASKS = os.environ['modal.state.machineMasks']
+THICKNESS = int(os.environ['modal.state.thickness'])
 
+# fix чтобы не падало +
+# modal window
+# readme (simple)
 
-@my_app.callback("do_something")
+@my_app.callback("export_as_masks")
 @sly.timeit
-def do(api: sly.Api, task_id, context, state, app_logger):
+def export_as_masks(api: sly.Api, task_id, context, state, app_logger):
     project_info = api.project.get_info_by_id(PROJECT_ID)
 
     dataset_ids = [ds.id for ds in api.dataset.get_list(PROJECT_ID)]
@@ -33,44 +36,57 @@ def do(api: sly.Api, task_id, context, state, app_logger):
     sly.download_project(api, project_info.id, dest_dir, dataset_ids=dataset_ids, log_progress=True)
     sly.logger.info('Project {!r} has been successfully downloaded. Starting to render masks.'.format(project_info.name))
 
-    project = sly.Project(directory=dest_dir, mode=sly.OpenMode.READ)
-    machine_colors = {obj_class.name: [idx, idx, idx] for idx, obj_class in
-                      enumerate(project.meta.obj_classes, start=1)}
-    dump_json_file(machine_colors, os.path.join(dest_dir, 'obj_class_to_machine_color.json'), indent=2)
-    for dataset in project:
-        ds_progress = sly.Progress(
-            'Processing dataset: {!r}/{!r}'.format(project.name, dataset.name), total_cnt=len(dataset))
+    if MACHINE_MASKS == 'enable' or HUMAN_MASKS == 'enable':
+        project = sly.Project(directory=dest_dir, mode=sly.OpenMode.READ)
 
-        human_masks_dir = os.path.join(dataset.directory, 'masks_human')
-        machine_masks_dir = os.path.join(dataset.directory, 'masks_machine')
-        sly.fs.mkdir(human_masks_dir)
-        sly.fs.mkdir(machine_masks_dir)
-        for item_name in dataset:
-            item_paths = dataset.get_item_paths(item_name)
-            ann = sly.Annotation.load_json_file(item_paths.ann_path, project.meta)
-            mask_img_name = os.path.splitext(item_name)[0] + '.png'
+        if MACHINE_MASKS:
+            machine_colors = {obj_class.name: [idx, idx, idx] for idx, obj_class in
+                              enumerate(project.meta.obj_classes, start=1)}
+            dump_json_file(machine_colors, os.path.join(dest_dir, 'obj_class_to_machine_color.json'), indent=2)
 
-            # Render and save human interpretable masks.
-            raw_img = sly.image.read(item_paths.img_path)
-            raw_img_rendered = raw_img.copy()
-            for label in ann.labels:
-                label.geometry.draw(raw_img_rendered,
-                                    color=label.obj_class.color,
-                                    config=label.obj_class.geometry_config,
-                                    thickness=thickness)
-            raw_img_rendered = ((raw_img_rendered.astype(np.uint16) + raw_img.astype(np.uint16)) / 2).astype(np.uint8)
-            sly.image.write(os.path.join(human_masks_dir, mask_img_name),
-                            np.concatenate([raw_img, raw_img_rendered], axis=1))
+        for dataset in project:
+            ds_progress = sly.Progress(
+                'Processing dataset: {!r}/{!r}'.format(project.name, dataset.name), total_cnt=len(dataset))
 
-            # Render and save machine readable masks.
-            machine_mask = np.zeros(shape=ann.img_size + (3,), dtype=np.uint8)
-            for label in ann.labels:
-                label.geometry.draw(machine_mask, color=machine_colors[label.obj_class.name], thickness=thickness)
-            sly.image.write(os.path.join(machine_masks_dir, mask_img_name), machine_mask)
+            if HUMAN_MASKS == 'enable':
+                human_masks_dir = os.path.join(dataset.directory, 'masks_human')
+                sly.fs.mkdir(human_masks_dir)
 
-            ds_progress.iter_done_report()
+            if MACHINE_MASKS == 'enable':
+                machine_masks_dir = os.path.join(dataset.directory, 'masks_machine')
+                sly.fs.mkdir(machine_masks_dir)
 
-    sly.logger.info('Finished masks rendering.'.format(project_info.name))
+            for item_name in dataset:
+                item_paths = dataset.get_item_paths(item_name)
+                ann = sly.Annotation.load_json_file(item_paths.ann_path, project.meta)
+
+                if MACHINE_MASKS == 'enable' or HUMAN_MASKS == 'enable':
+                    mask_img_name = os.path.splitext(item_name)[0] + '.png'
+
+                # Render and save human interpretable masks.
+                raw_img = sly.image.read(item_paths.img_path)
+                raw_img_rendered = raw_img.copy()
+
+                if HUMAN_MASKS == 'enable':
+                    for label in ann.labels:
+                        label.geometry.draw(raw_img_rendered,
+                                            color=label.obj_class.color,
+                                            config=label.obj_class.geometry_config,
+                                            thickness=THICKNESS)
+                    raw_img_rendered = ((raw_img_rendered.astype(np.uint16) + raw_img.astype(np.uint16)) / 2).astype(np.uint8)
+                    sly.image.write(os.path.join(human_masks_dir, mask_img_name),
+                                    np.concatenate([raw_img, raw_img_rendered], axis=1))
+
+                # Render and save machine readable masks.
+                if MACHINE_MASKS == 'enable':
+                    machine_mask = np.zeros(shape=ann.img_size + (3,), dtype=np.uint8)
+                    for label in ann.labels:
+                        label.geometry.draw(machine_mask, color=machine_colors[label.obj_class.name], thickness=THICKNESS)
+                    sly.image.write(os.path.join(machine_masks_dir, mask_img_name), machine_mask)
+
+                    ds_progress.iter_done_report()
+
+        sly.logger.info('Finished masks rendering.'.format(project_info.name))
 
     my_app.stop()
 
@@ -82,7 +98,7 @@ def main():
         "context.workspaceId": WORKSPACE_ID
     })
 
-    initial_events = [{"state": None, "context": None, "command": "do_something"}]
+    initial_events = [{"state": None, "context": None, "command": "export_as_masks"}]
 
     # Run application service
     my_app.run(initial_events=initial_events)
