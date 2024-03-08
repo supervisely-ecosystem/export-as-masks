916843240a6c651e2fc12197a629ba744cba49b1
iwatkot <iwatkot@gmail.com>
2024-03-08 16:35:35 +0300

    Nested datasets.

--- src/globals.py
+++ src/globals.py
@@ -4,12 +4,10 @@ from distutils.util import strtobool
 
 import supervisely as sly
 from dotenv import load_dotenv
-
-
 from PIL import Image
-from supervisely.app.v1.app_service import AppService
 
-app_root_directory = os.path.dirname(os.getcwd())
+cwd = os.getcwd()
+app_root_directory = os.path.dirname(cwd)
 sys.path.append(app_root_directory)
 sys.path.append(os.path.join(app_root_directory, "src"))
 print(f"App root directory: {app_root_directory}")
@@ -19,12 +17,13 @@ if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-TASK_ID = os.environ["TASK_ID"]
 TEAM_ID = int(os.environ["context.teamId"])
 WORKSPACE_ID = int(os.environ["context.workspaceId"])
 PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
 
-my_app = AppService()
+sly.logger.info(f"Team ID: {TEAM_ID}, Workspace ID: {WORKSPACE_ID}, Project ID: {PROJECT_ID}")
+
+api = sly.Api.from_env()
 
 Image.MAX_IMAGE_PIXELS = None
 HUMAN_MASKS = bool(strtobool(os.environ["modal.state.humanMasks"]))
@@ -32,12 +31,17 @@ MACHINE_MASKS = bool(strtobool(os.environ["modal.state.machineMasks"]))
 INSTANCE_MASKS = bool(strtobool(os.environ["modal.state.instanceMasks"]))
 THICKNESS = int(os.environ["modal.state.thickness"])
 
+sly.logger.info(
+    f"Export human masks: {HUMAN_MASKS}, machine masks: {MACHINE_MASKS}, "
+    f"instance masks: {INSTANCE_MASKS}, thickness: {THICKNESS}"
+)
+
 SIZE_LIMIT = 10 if sly.is_community() else 100
 SIZE_LIMIT_BYTES = SIZE_LIMIT * 1024 * 1024 * 1024
 SPLIT_MODE = "MB"
-SPLIT_SIZE = 500 # do not increase this value (memory issues)
+SPLIT_SIZE = 500  # do not increase this value (memory issues)
 # SPLIT_SIZE_BYTES = SPLIT_SIZE * 1024 * 1024 * 1024
 
-STORAGE_DIR = sly.app.get_data_dir()
-if sly.fs.dir_exists(STORAGE_DIR):
-    sly.fs.clean_dir(STORAGE_DIR)
+STORAGE_DIR = os.path.join(cwd, "storage")
+sly.fs.mkdir(STORAGE_DIR, remove_content_if_exists=True)
+sly.logger.debug(f"Storage directory: {STORAGE_DIR}")
